0e0a0c81a2d497f57bcfa3d541d0c9d3d784cb59
Fengwangdong Zhang <fengwang@fengmac.dyndns.cern.ch>
2018-07-11 10:54:24 +0200

    update the azimuthal maps

--- tempPlotter.py
+++ tempPlotter.py
@@ -10,12 +10,10 @@ ROOT.gStyle.SetErrorX(0.5)
 inputFileName = "temperatures.txt"
 layer = ["L1","L2","L3","L4"]
 
-dataPlus = {}
-dataMinus = {}
+data = {}
 
 for j,index in enumerate(layer):
-    dataPlus[j] = [array('d'), array('d'), array('d'), array('d')]
-    dataMinus[j] = [array('d'), array('d'), array('d'), array('d')]
+    data[j] = [array('d'), array('d'), array('d'), array('d')]
 
 if(os.path.exists(inputFileName)):
     fin = open(inputFileName, "r+")
@@ -27,27 +25,24 @@ if(os.path.exists(inputFileName)):
         for i,ilayer in enumerate(layer):
 
             if ilayer in line[0]:
-                if ("P" in line[0]):
-                    dataPlus[i][0].append(float(line[1]))
-                    dataPlus[i][1].append(float(line[2]))
-                    dataPlus[i][2].append(float(line[3]))
-                    dataPlus[i][3].append(45) # constant unc. for the phi
+                data[i][0].append(float(line[1]))
+                data[i][1].append(float(line[2]))
+                data[i][2].append(float(line[3]))
 
-                elif("M" in line[0]):
-                    dataMinus[i][0].append(float(line[1]))
-                    dataMinus[i][1].append(float(line[2]))
-                    dataMinus[i][2].append(float(line[3]))
-                    dataMinus[i][3].append(45) # constant unc. for the phi
+                if (ilayer == "L1" or ilayer == "L2"):
+                    data[i][3].append(45) # constant unc. for the phi
+
+                else:
+                    data[i][3].append(22.5) # constant unc. for the phi
 
             else:
                 continue
 
 
-    if len(dataPlus) > 0 and len(dataMinus) > 0:
+    if len(data) > 0:
 
         for k,klayer in enumerate(layer):
-            gr_plus = ROOT.TGraphAsymmErrors(len(dataPlus[k][0]), dataPlus[k][0], dataPlus[k][1], dataPlus[k][3], dataPlus[k][3], dataPlus[k][2], dataPlus[k][2])
-            gr_minus = ROOT.TGraphAsymmErrors(len(dataMinus[k][0]), dataMinus[k][0], dataMinus[k][1], dataMinus[k][3], dataMinus[k][3], dataMinus[k][2], dataMinus[k][2])
+            gr_temp = ROOT.TGraphAsymmErrors(len(data[k][0]), data[k][0], data[k][1], data[k][3], data[k][3], data[k][2], data[k][2])
 
             canvas = ROOT.TCanvas("temperatures","temperatures",900,900)
             canvas.SetTopMargin(0.05)
@@ -58,9 +53,7 @@ if(os.path.exists(inputFileName)):
             frameHist = ROOT.TH1D("temperatures","temperatures", 16, 0, 360)
             frameHist.SetStats(0)
 
-            maxValue = max(dataPlus[k][1])
-            if maxValue < max(dataMinus[k][1]):
-                maxValue = max(dataMinus[k][1])
+            maxValue = max(data[k][1])
 
             frameHist.GetYaxis().SetRangeUser(-15,-4)
             frameHist.GetYaxis().SetTitle("Temperature [C]")
@@ -74,17 +67,11 @@ if(os.path.exists(inputFileName)):
 
             frameHist.Draw()
 
-            gr_plus.SetLineColor(2)
-            gr_plus.SetLineWidth(4)
-            gr_plus.SetMarkerSize(4)
-            gr_plus.SetMarkerStyle(20)
-            gr_plus.SetMarkerColor(2)
-
-            gr_minus.SetLineColor(4)
-            gr_minus.SetLineWidth(4)
-            gr_minus.SetMarkerSize(3)
-            gr_minus.SetMarkerStyle(21)
-            gr_minus.SetMarkerColor(4)
+            gr_temp.SetLineColor(2)
+            gr_temp.SetLineWidth(4)
+            gr_temp.SetMarkerSize(4)
+            gr_temp.SetMarkerStyle(20)
+            gr_temp.SetMarkerColor(2)
 
             label = ROOT.TLatex(0.21,0.9, klayer.replace("L","LYR"))
             label.SetNDC()
@@ -92,11 +79,9 @@ if(os.path.exists(inputFileName)):
             legend = ROOT.TLegend(0.7,0.87,0.99,0.99)
             legend.SetFillColor(0)
             legend.SetTextSize(0.04)
-            legend.AddEntry(gr_plus,"BPix_plus","lpe")
-            legend.AddEntry(gr_minus,"BPix_minus","lpe")
+            legend.AddEntry(gr_temp,"BPix","lpe")
 
-            gr_plus.Draw("ep same")
-            gr_minus.Draw("ep same")
+            gr_temp.Draw("ep same")
             legend.Draw("same")
             label.Draw("same")
             canvas.SaveAs("BPix_" + klayer + "_temperature.pdf")
