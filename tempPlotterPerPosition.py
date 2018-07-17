b1f6917203d33f77b089a71ca46e2c3f4b9f8125
Fengwangdong Zhang <fengwang@fengmac.dyndns.cern.ch>
2018-07-17 18:00:53 +0200

    temperature 2D plots

---
+++ tempPlotterPerPosition.py
@@ -0,0 +1,108 @@
+import ROOT
+from ROOT import TStyle, TH2, TH2F, TCanvas, TROOT, TLatex, TAttFill, TColor
+import tdrStyle
+import os
+from array import array
+
+tdrStyle.setTDRStyle()
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetPaintTextFormat("4.1f")
+ROOT.gStyle.SetPalette(87)
+
+loopnames = [
+[
+"L1D1PN",
+"L1D2MN",
+"L1D1MF",
+"L1D2PF",
+],
+[
+"L2D1MN",
+"L2D2PN",
+"L2D1PF",
+"L2D2MF",
+],
+[
+"L3D1MN",
+"L3D2PN",
+"L3D3MN",
+"L3D4PN",
+"L3D1PF",
+"L3D2MF",
+"L3D3PF",
+"L3D4MF",
+],
+[
+"L4D1PN",
+"L4D2MN",
+"L4D3PN",
+"L4D4MN",
+"L4D1MF",
+"L4D2PF",
+"L4D3MF",
+"L4D4PF",
+],
+]
+
+
+inputFileName = "temperatures_perposition.txt"
+layer = ["L1","L2","L3","L4"]
+
+if(os.path.exists(inputFileName)):
+    fin = open(inputFileName, "r+")
+    lines = fin.readlines()
+
+    for i,ilayer in enumerate(layer):
+        frameHist = ROOT.TH2F("temp","temp",len(loopnames[i]),0,len(loopnames[i]),3,0,3)
+        frameHist.GetYaxis().SetBinLabel(1,"begin")
+        frameHist.GetYaxis().SetBinLabel(2,"middle")
+        frameHist.GetYaxis().SetBinLabel(3,"end")
+
+        for iloop in xrange(len(loopnames[i])):
+            frameHist.GetXaxis().SetBinLabel(iloop+1, loopnames[i][iloop])
+
+
+            for l in lines:
+                line = l.split()
+
+                if (loopnames[i][iloop] in line[0]) and (line[1]!="null"):
+
+                    if "I_" in line[0]:
+                        frameHist.SetBinContent(iloop+1,1,float(line[1]))
+
+                    elif "M_" in line[0]:
+                        frameHist.SetBinContent(iloop+1,2,float(line[1]))
+
+                    elif "R_" in line[0]:
+                        frameHist.SetBinContent(iloop+1,3,float(line[1]))
+
+                else:
+                    continue
+
+
+        canvas = ROOT.TCanvas("temperatures","temperatures",900,900)
+        canvas.SetTopMargin(0.07)
+        canvas.SetLeftMargin(0.15)
+        canvas.SetRightMargin(0.15)
+        canvas.SetBottomMargin(0.16)
+        canvas.cd()
+
+
+        frameHist.GetYaxis().SetTitle("position")
+        frameHist.GetYaxis().SetTitleOffset(1.)
+        frameHist.GetYaxis().SetTitleSize(0.06)
+        frameHist.GetYaxis().SetLabelSize(0.05)
+
+        frameHist.GetXaxis().SetTitle("cooling loop")
+        frameHist.GetXaxis().SetTitleSize(0.07)
+        frameHist.GetXaxis().SetLabelSize(0.05)
+
+        frameHist.GetZaxis().SetLabelSize(0.05)
+        frameHist.SetMarkerSize(1.7)
+        frameHist.Draw("colztext")
+
+        label = ROOT.TLatex(0.21,0.95, ilayer.replace("L","LYR") + "   temperature [C]")
+        label.SetNDC()
+
+        label.Draw("same")
+        canvas.SaveAs("BPix_" + ilayer + "_temperature_2D.pdf")
---
+++ tempReaderPerPosition.py
@@ -0,0 +1,109 @@
+import os
+import ROOT
+from ROOT import TH1, TH1F
+
+validProbe = [
+"3I_L1D1MF",
+"3M_L1D1MF",
+"3R_L1D1MF",
+"4M_L2D2MF",
+"4R_L2D2MF",
+"2I_L3D2MF",
+"2M_L3D2MF",
+"2R_L3D2MF",
+"5I_L3D4MF",
+"5M_L3D4MF",
+"5R_L3D4MF",
+"1I_L4D1MF",
+"1M_L4D1MF",
+"1R_L4D1MF",
+"6I_L4D3MF",
+"6M_L4D3MF",
+"6R_L4D3MF",
+"4I_L1D2PF",
+"4R_L1D2PF",
+"3I_L2D1PF",
+"3M_L2D1PF",
+"3R_L2D1PF",
+"2I_L3D1PF",
+"2M_L3D1PF",
+"2R_L3D1PF",
+"5I_L3D3PF",
+"5M_L3D3PF",
+"5R_L3D3PF",
+"1I_L4D2PF",
+"1M_L4D2PF",
+"1R_L4D2PF",
+"6I_L4D4PF",
+"6M_L4D4PF",
+"6R_L4D4PF",
+"4I_L1D2MN",
+"4M_L1D2MN",
+"4R_L1D2MN",
+"3I_L2D1MN",
+"3M_L2D1MN",
+"3R_L2D1MN",
+"2I_L3D1MN",
+"2R_L3D1MN",
+"5I_L3D3MN",
+"5M_L3D3MN",
+"5R_L3D3MN",
+"1I_L4D2MN",
+"1M_L4D2MN",
+"1R_L4D2MN",
+"6I_L4D4MN",
+"6M_L4D4MN",
+"6R_L4D4MN",
+"3I_L1D1PN",
+"3R_L1D1PN",
+"4M_L2D2PN",
+"4R_L2D2PN",
+"2I_L3D2PN",
+"2M_L3D2PN",
+"2R_L3D2PN",
+"5I_L3D4PN",
+"5M_L3D4PN",
+"5R_L3D4PN",
+"1I_L4D1PN",
+"1M_L4D1PN",
+"1R_L4D1PN",
+"6I_L4D3PN",
+"6M_L4D3PN",
+"6R_L4D3PN"]
+
+
+fileName = "temperatureFromDB_Corr.txt"
+outputFile = "temperatures_perposition.txt"
+
+if(os.path.exists(fileName)):
+    fin = open(fileName, "r+")
+    lines = fin.readlines()
+
+    fout = open(outputFile,"w")
+
+    for index in validProbe:
+
+        tempHist = ROOT.TH1F("temperature", "temperature", 200, -20, 0)
+
+        for l in lines:
+            line = l.split()
+
+            if (index in line[0]):
+                tempHist.Fill(float(line[1]))
+
+            else:
+                continue
+
+        if tempHist.GetEntries() == 0:
+            fout.write(index + "   " + "null" + "   " + "null" + "\n")
+
+        else:
+            aveTemp = tempHist.GetMean()
+            uncTemp = tempHist.GetStdDev()
+            fout.write(index + "   " + str(aveTemp) + "   " + str(uncTemp) + "\n")
+
+        tempHist.Reset()
+
+
+    fin.close()
+    fout.close()
