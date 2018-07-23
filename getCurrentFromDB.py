f8fc3f6cc86d9d344b84e98519a3996bd583676a
Fengwangdong Zhang <fengwang@fengmac.dyndns.cern.ch>
2018-07-23 18:30:22 +0200

    fetch the last current points during selected time range

--- getCurrentFromDB.py
+++ getCurrentFromDB.py
@@ -6,74 +6,67 @@ import datetime
 connection = cx_Oracle.connect('cms_trk_r/1A3C5E7G:FIN@cms_omds_adg')
 cursor = connection.cursor()
 
-start_time = "2018-07-18 04:12:00"
+start_time = "2018-07-18 00:12:00"
 stop_time = "2018-07-18 10:59:00"
 
-option = "PixelBarrel"
-#query = "select dpe_name, alias, id from cms_trk_dcs_pvss_cond.aliases join cms_trk_dcs_pvss_cond.dp_name2id on (dpname || '.' = dpe_name) where alias like '%%/%s'" % (option + "%%channel%%")
+options = "PixelBarrel"
 
-query = """select distinct substr(lal.alias,INSTR(lal.alias,  '/', -1, 2)+1), id, lal.dpe_name, cd from
+query = """
+with cables as (
+    select distinct substr(lal.alias,INSTR(lal.alias, '/', -1, 2)+1) as cable, id, cd from
     (select max(since) as cd, alias from  cms_trk_dcs_pvss_cond.aliases group by alias) md, cms_trk_dcs_pvss_cond.aliases lal
     join cms_trk_dcs_pvss_cond.dp_name2id on dpe_name=concat(dpname,'.')
     where md.alias=lal.alias and lal.since=cd
-    and (lal.alias like '%%/%s')""" % (option + "%%channel%%")
+    and (lal.alias like '""" + str("%%" + options + "%%channel002%%") + """'
+    or lal.alias like '""" + str("%%" + options + "%%channel003%%") + """')
+),
 
-print "start executing query 1..."
+it as (
+    select dpid, max(change_date) as itime from cms_trk_dcs_pvss_cond.fwcaenchannel
+    where change_date >= to_timestamp('""" + str(start_time) + """', 'RRRR-MM-DD HH24:MI:SS.FF')
+    and change_date <= to_timestamp('""" + str(stop_time) + """','RRRR-MM-DD HH24:MI:SS.FF')
+    and actual_Imon is not NULL
+    group by dpid
+)
+
+select cable, actual_Imon, change_date from cms_trk_dcs_pvss_cond.fwcaenchannel, cables, it
+where change_date >= to_timestamp('""" + str(start_time) + """', 'RRRR-MM-DD HH24:MI:SS.FF')
+and change_date <= to_timestamp('""" + str(stop_time) + """','RRRR-MM-DD HH24:MI:SS.FF')
+and actual_Imon is not NULL
+and cms_trk_dcs_pvss_cond.fwcaenchannel.dpid = it.dpid
+and it.dpid = cables.id
+and change_date = it.itime
+order by cable, change_date
+"""
+
+print "executing query ..."
 cursor.execute(query)
-dpid_rows = cursor.fetchall()
-print "finish query 1 execution!"
+result = cursor.fetchall()
+print "finish query execution!"
 
 outputFile = "currentsFromDB.txt"
 fcur = open(outputFile, "w+")
 
-for k in xrange(len(dpid_rows)):
-    query2 = """select actual_Imon, change_date from cms_trk_dcs_pvss_cond.fwcaenchannel
-    where change_date between TO_TIMESTAMP('%s', 'RRRR-MM-DD HH24:MI:SS.FF') and TO_TIMESTAMP('%s', 'RRRR-MM-DD HH24:MI:SS.FF')
-    and dpid='%s' and actual_Imon is not NULL
-            order by change_date""" % (start_time, stop_time, dpid_rows[k][1])
+print "writing output..."
 
-    print "executing query 2..."
-    cursor.execute(query2)
-    result = cursor.fetchall()
-    print "finish query 2 execution!"
-    print "writing output..."
+for i in xrange(len(result)):
 
-    dpid = str(dpid_rows[k][0])
+    if ("LAY1/" in result[i][0]) or ("LAY3/" in result[i][0]):
+        continue
 
-    if len(result) > 0:
+    if "LAY14/channel002" in result[i][0]:
+        dpid = result[i][0].replace("LAY14/channel002","LAY1")
 
-        if ("channel000" in dpid) or ("channel001" in dpid):
-            continue
+    if "LAY14/channel003" in result[i][0]:
+        dpid = result[i][0].replace("LAY14/channel003","LAY4")
 
-        if ("LAY1/" in dpid) or ("LAY3/" in dpid):
-            continue
+    if "LAY23/channel002" in result[i][0]:
+        dpid = result[i][0].replace("LAY23/channel002","LAY3")
 
-        else:
-            dpid = dpid.replace("LAY14/channel002","LAY1")
-            dpid = dpid.replace("LAY14/channel003","LAY4")
-            dpid = dpid.replace("LAY23/channel002","LAY3")
-            dpid = dpid.replace("LAY23/channel003","LAY2")
-            recordValue = result[0][0]
-            fcur.write(dpid + "   " + str(result[0][0]) + "   " + str(result[0][1]) + "\n")
+    if "LAY23/channel003" in result[i][0]:
+        dpid = result[i][0].replace("LAY23/channel003","LAY2")
 
-    for i in xrange(len(result)):
-
-        if ("channel000" in dpid) or ("channel001" in dpid):
-            continue
-
-        if ("LAY1/" in dpid) or ("LAY3/" in dpid):
-            continue
-
-        if (abs(recordValue - result[i][0]) <= 3.0):
-            continue
-
-        else:
-            dpid = dpid.replace("LAY14/channel002","LAY1")
-            dpid = dpid.replace("LAY14/channel003","LAY4")
-            dpid = dpid.replace("LAY23/channel002","LAY3")
-            dpid = dpid.replace("LAY23/channel003","LAY2")
-            recordValue = result[i][0]
-            fcur.write(dpid + "   " + str(result[i][0]) + "   " + str(result[i][1]) + "\n")
+    fcur.write(dpid + "   " + str(result[i][1]) + "   " + str(result[i][2]) + "\n")
 
 print "finish output!"
 
